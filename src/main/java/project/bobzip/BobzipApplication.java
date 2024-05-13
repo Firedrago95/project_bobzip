fa573d16cb3eb37594c2c8cb38af0d4541cbb681
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-13 14:00:57 +0900

    initial commit

---
+++ src/main/java/project/bobzip/BobzipApplication.java
@@ -0,0 +1,13 @@
+package project.bobzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/bobzip/BobzipApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bobzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
