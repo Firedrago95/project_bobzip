d20e53003ac1f2aa1dcc87fb39beb1843c9c230b
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-13 19:52:24 +0900

    회원 엔티티 작성

---
+++ src/main/java/project/bobzip/member/entity/Member.java
@@ -0,0 +1,26 @@
+package project.bobzip.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String userId;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+}
