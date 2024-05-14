d05ec197f22dbe0588871bdc8f371c508d3e4447
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-14 18:49:29 +0900

    회원가입 테스트 작성

---
+++ src/test/java/project/bobzip/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package project.bobzip.member.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.bobzip.member.entity.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void saveTest() {
+        Member member = new Member();
+        member.setUsername("userA");
+        member.setUserId("abcd");
+        member.setPassword("1234");
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
