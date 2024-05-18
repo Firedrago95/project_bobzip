5c19674e0cf77544d8f3a82b8a02f977568f1517
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-18 17:59:30 +0900

    로그인 기능 완성

--- src/main/java/project/bobzip/global/controller/HomeController.java
+++ src/main/java/project/bobzip/global/controller/HomeController.java
@@ -1,13 +1,20 @@
 package project.bobzip.global.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import project.bobzip.member.entity.Member;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String home() {
+    public String home(@SessionAttribute(name = "loginMember", required = false) Member loginMember,
+                       Model model) {
+        model.addAttribute("loginMember", loginMember);
         return "home";
     }
 }
--- src/main/java/project/bobzip/member/controller/LoginController.java
+++ src/main/java/project/bobzip/member/controller/LoginController.java
@@ -1,5 +1,7 @@
 package project.bobzip.member.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -26,16 +28,22 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@Validated @ModelAttribute("memberForm") LoginForm loginForm,
-                        BindingResult bindingResult) {
+                        BindingResult bindingResult,
+                        HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "members/loginForm";
         }
+
         Member loginMember = memberService.login(loginForm.getUserId(), loginForm.getPassword());
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "members/loginForm";
         }
-        return null;
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loginMember", loginMember);
+
+        return "redirect:/";
     }
 
 }
