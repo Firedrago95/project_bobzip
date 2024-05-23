45eb7c220573aa5976ae761b943bb33f8bf711df
YONGHWA\ekore <yonghwa95@naver.com>
2024-05-23 14:53:45 +0900

    enum 작성

--- src/main/java/project/bobzip/recipe/entity/IngredientCategory.java
+++ src/main/java/project/bobzip/recipe/entity/IngredientCategory.java
@@ -4,4 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum IngredientCategory {
+    CONDIMENT("조미료"),
+    VEGETABLE("채소"),
+    MEAT("육류"),
+    FRUITS("과일"),
+    GRAIN("곡물"),
+    SEAFOOD("해산물"),
+    MUSHROOM("버섯");
+
+    private final String categoryName;
+
+    IngredientCategory(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
--- src/main/java/project/bobzip/recipe/entity/Unit.java
+++ src/main/java/project/bobzip/recipe/entity/Unit.java
@@ -4,4 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum Unit {
+    g("g"),
+    ml("ml"),
+    kg("kg"),
+    L("L");
+
+    private final String symbol;
+
+    Unit(String symbol) {
+        this.symbol = symbol;
+    }
 }
