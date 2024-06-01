c023bda9516fc6c0ab1b1c3908f92e2e15a40ae8
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-01 11:12:40 +0900

    docs 레시피 등록 정보 검증 기능목록 추가

--- src/main/java/project/bobzip/recipe/repository/RecipeIngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package project.bobzip.recipe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.bobzip.recipe.entity.RecipeIngredient;
-
-public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
-}
--- src/main/java/project/bobzip/recipe/repository/RecipeStepRepository.java
+++
@@ -1,7 +0,0 @@
-package project.bobzip.recipe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.bobzip.recipe.entity.RecipeStep;
-
-public interface RecipeStepRepository extends JpaRepository<RecipeStep, Long> {
-}
