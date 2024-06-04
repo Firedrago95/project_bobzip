da83403953d11aeec32a050c7ff371d0c7ecb5b8
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-04 13:58:19 +0900

    로그인 검증 추가

---
+++ src/main/java/project/bobzip/member/dto/LoginConst.java
@@ -0,0 +1,5 @@
+package project.bobzip.member.dto;
+
+public class LoginConst {
+    public static final String LOGIN = "loginMember";
+}
--- src/main/java/project/bobzip/recipe/controller/RecipeController.java
+++ src/main/java/project/bobzip/recipe/controller/RecipeController.java
@@ -1,5 +1,6 @@
 package project.bobzip.recipe.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import project.bobzip.member.dto.LoginConst;
 import project.bobzip.member.entity.Member;
 import project.bobzip.recipe.dto.RecipeForm;
 import project.bobzip.recipe.entity.Unit;
@@ -33,15 +35,20 @@ public class RecipeController {
     }
 
     @GetMapping("/add")
-    public String recipeForm(@ModelAttribute("recipeForm")RecipeForm recipeForm) {
+    public String recipeForm(@ModelAttribute("recipeForm")RecipeForm recipeForm,
+                             @SessionAttribute(name = LoginConst.LOGIN, required = false) Member loginMember) {
+        // 로그인 검증
+        if (loginMember == null) {return "redirect:/members/login";}
         return "/recipe/recipeForm";
     }
 
     @PostMapping("/add")
     public String addRecipe(@Validated @ModelAttribute RecipeForm recipeForm,
                             BindingResult bindingResult,
-                            @SessionAttribute(name = "loginMember", required = true) Member loginMember) throws IOException {
-        // 필드별 미입력 검증
+                            @SessionAttribute(name = LoginConst.LOGIN, required = false) Member loginMember) throws IOException {
+        // 로그인 검증
+        if (loginMember == null) {return "redirect:/members/login";}
+        // 입력 정보 오류 있다면 입력폼으로
         if (bindingResult.hasErrors()) {
             return "/recipe/recipeForm";
         }
