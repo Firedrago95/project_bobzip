7a7c32538591d8800ec0abd8940250ed25e3fdb9
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-08 15:04:44 +0900

    fix : 엔티티 생성방법 변경

--- src/main/java/project/bobzip/recipe/service/RecipeService.java
+++ src/main/java/project/bobzip/recipe/service/RecipeService.java
@@ -2,13 +2,15 @@ package project.bobzip.recipe.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import project.bobzip.global.entity.UploadFile;
 import project.bobzip.global.util.fileupload.FileStore;
 import project.bobzip.member.entity.Member;
-import project.bobzip.recipe.dto.RecipeForm;
+import project.bobzip.recipe.dto.RecipeAddForm;
 import project.bobzip.recipe.entity.*;
 import project.bobzip.recipe.repository.IngredientRepository;
 import project.bobzip.recipe.repository.RecipeRepository;
@@ -27,32 +29,31 @@ public class RecipeService {
     private final FileStore fileStore;
 
     @Transactional
-    public void addRecipe(RecipeForm recipeForm, Member member) throws IOException {
+    public void addRecipe(RecipeAddForm recipeAddForm, Member member) throws IOException {
         // 파일 저장
-        MultipartFile thumbnail = recipeForm.getThumbnail();
-        List<MultipartFile> stepThumbnails = recipeForm.getStepThumbnails();
+        MultipartFile thumbnail = recipeAddForm.getThumbnail();
+        List<MultipartFile> stepThumbnails = recipeAddForm.getStepThumbnails();
         UploadFile recipeThumbnailUrl = fileStore.addThumbnail(thumbnail);
         List<UploadFile> stepThumbnailUrls = fileStore.addThumbnail(stepThumbnails);
 
         // 재료 생성
-        List<Ingredient> ingredients = Ingredient.createIngredients(recipeForm.getIngredientNames());
+        List<Ingredient> ingredients = Ingredient.createIngredients(recipeAddForm.getIngredientNames());
         ingredientRepository.saveAll(ingredients);
 
         // 레시피 재료 생성
         List<RecipeIngredient> recipeIngredients = RecipeIngredient.createRecipeIngredient(
                 ingredients,
-                recipeForm.getQuantities(),
-                recipeForm.getUnits());
+                recipeAddForm.getQuantities(),
+                recipeAddForm.getUnits());
 
         // 레시피 단계 생성
         List<RecipeStep> recipeSteps = RecipeStep.createRecipeSteps(
                 stepThumbnailUrls,
-                recipeForm.getStepInstructions());
+                recipeAddForm.getStepInstructions());
 
         // 레시피 생성
-        Recipe recipe = Recipe.createRecipe(recipeIngredients, recipeSteps, recipeForm.getInstruction(),
-                member, recipeForm.getTitle(), recipeThumbnailUrl);
+        Recipe recipe = Recipe.createRecipe(recipeIngredients, recipeSteps, recipeAddForm.getInstruction(),
+                member, recipeAddForm.getTitle(), recipeThumbnailUrl);
         recipeRepository.save(recipe);
     }
-
 }
