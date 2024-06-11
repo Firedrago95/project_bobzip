8e4a8780b9525195ee6d2df3ecdb9b836d353f98
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-11 11:32:55 +0900

    test : 회원탈퇴 및 MemberService 테스트코드 작성

--- src/test/java/project/bobzip/member/service/MemberServiceTest.java
+++ src/test/java/project/bobzip/member/service/MemberServiceTest.java
@@ -17,4 +17,52 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class MemberServiceTest {
 
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+    
+    @Test
+    void addTest() {
+        // given
+        Member member = new Member("abc", "123", "yong");
+
+        // when
+        memberService.add(member);
+        
+        // then
+        Member findMember = memberRepository.findByUserId("abc").get();
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void loginTest() {
+        // given
+        Member registeredMember = new Member("register", "123", "member");
+        memberService.add(registeredMember);
+        Member unRegisteredMember = new Member("unRegister", "456", "notMember");
+
+        // when
+        Member loginMember1 = memberService.login(registeredMember.getUserId(), registeredMember.getPassword());
+        Member loginMember2 = memberService.login(unRegisteredMember.getUserId(), unRegisteredMember.getPassword());
+
+        // then
+        assertThat(loginMember1).isEqualTo(registeredMember);
+        assertThat(loginMember2).isNull();
+    }
+
+    @Test
+    void deleteTest() {
+        // given
+        Member member = new Member("abc", "123", "yong");
+        memberService.add(member);
+
+        // when
+        memberService.delete(member);
+
+        // then
+        Optional<Member> findMember = memberRepository.findByUserId("abc");
+        assertThat(findMember).isEmpty();
+    }
 }
