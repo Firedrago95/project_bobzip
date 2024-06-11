14eff78c5919ffc524974885b263a35db67ec633
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-11 11:12:42 +0900

    test: MemberRepository  delete 테스트 작성

--- src/test/java/project/bobzip/member/repository/MemberRepositoryTest.java
+++ src/test/java/project/bobzip/member/repository/MemberRepositoryTest.java
@@ -1,11 +1,14 @@
 package project.bobzip.member.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import project.bobzip.member.entity.Member;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,21 +20,37 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    private Member testMember;
+
+    @BeforeEach
+    void initData() {
+        testMember = new Member("userA", "abcd", "1234");
+        memberRepository.save(testMember);
+    }
+
     @Test
     void saveTest() {
-        Member member = new Member("userA", "abcd", "1234");
-        memberRepository.save(member);
-
-        Member findMember = memberRepository.findById(member.getId()).get();
-        assertThat(findMember).isEqualTo(member);
+        // then
+        Member findMember = memberRepository.findById(testMember.getId()).get();
+        assertThat(findMember).isEqualTo(testMember);
     }
 
     @Test
     void findByUserIdTest() {
-        Member member = new Member("userA", "abcd", "1234");
-        memberRepository.save(member);
+        // when
+        Member loginMember = memberRepository.findByUserId("userA").get();
 
-        Member loginMember = memberRepository.findByUserId("abcd").get();
-        assertThat(loginMember).isEqualTo(member);
+        // then
+        assertThat(loginMember).isEqualTo(testMember);
+    }
+
+    @Test
+    void deleteMemberTest() {
+        // when
+        memberRepository.delete(testMember);
+
+        // then
+        Optional<Member> deletedMember = memberRepository.findByUserId("abcd");
+        assertThat(deletedMember).isEmpty();
     }
 }
