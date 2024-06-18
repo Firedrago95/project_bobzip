763d527ee02f72ca020f7dbfa91f24a9c2db0e0c
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-18 10:25:23 +0900

    refactor : 페이지네이션 코드 리팩토링

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,43 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const totalPages = parseInt(document.getElementById('pagination').dataset.totalPages);
+    const currentPage = parseInt(document.getElementById('pagination').dataset.currentPage);
+    generatePagination(totalPages, currentPage);
+});
+
 function generatePagination(totalPages, currentPage) {
     const paginationContainer = document.getElementById('pagination');
     paginationContainer.innerHTML = '';
 
-    // 첫번째 페이지와 4페이지 이상인 경우 '...' 표시
+    // 첫번째 페이지 생성
+    paginationContainer.innerHTML += createPageLink(1, currentPage === 1);
+
     if (totalPages > 1) {
-        if (currentPage == 1) {
-            paginationContainer.innerHTML += `<a href='?page=0' class="selected">1</a>`
-        } else {
-            paginationContainer.innerHTML += `<a href='?page=0'>1</a>`;
-        }
+        // 현재 페이지가 5 이상이면 ... 표시
         if (currentPage > 4) {
-            paginationContainer.innerHTML += `<span>...</span>`;
+            paginationContainer.innerHTML += createEllipsis();
         }
-    }
 
-    // 총 페이지 수 만큼 페이지 생성
-    for(let i = Math.max(2, currentPage - 3); i <= Math.min(totalPages - 1, currentPage + 3); i++) {
-        if (i == currentPage) {
-            paginationContainer.innerHTML += `<a href='?page=${i-1}' class="selected">${i}</a>`;
-        } else {
-            paginationContainer.innerHTML += `<a href='?page=${i-1}'>${i}</a>`;
+        // 페이지 생성 최종페이지 바로 앞 페이지까지
+        for (let i = Math.max(2, currentPage - 3); i <= Math.min(totalPages - 1, currentPage + 3); i++) {
+            paginationContainer.innerHTML += createPageLink(i, currentPage === i);
         }
-    }
 
-    // 마지막 페이지 생성
-    if (totalPages > 1) {
+        // 현재 페이지가 총 페이지 -3 보다 작으면 ... 표시
         if (currentPage < totalPages - 3) {
-            paginationContainer.innerHTML += `<span>...</span>`;
+            paginationContainer.innerHTML += createEllipsis();
         }
-        if (currentPage == totalPages) {
-            paginationContainer.innerHTML += `<a href='?page=${totalPages-1}' class="selected">${totalPages}</a>`;
-        } else {
-            paginationContainer.innerHTML += `<a href='?page=${totalPages-1}'>${totalPages}</a>`;
-        }
+
+        // 마지막 페이지 표시
+        paginationContainer.innerHTML += createPageLink(totalPages, totalPages === currentPage);
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const totalPages = document.getElementById('pagination').dataset.totalPages;
-    const currentPage = document.getElementById('pagination').dataset.currentPage;
-    generatePagination(parseInt(totalPages), parseInt(currentPage));
-});
+function createPageLink(pageNum, isCurrent = false) {
+    return `<a href='?page=${pageNum - 1}' class='${isCurrent ? "selected" : ""}'>${pageNum}</a>`
+}
+
+function createEllipsis() {
+    return `<span>...</span>`
+}
+
