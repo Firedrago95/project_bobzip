b84da0ff8e8605d058f535d1cd16ce8901b737d2
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-21 09:19:00 +0900

    test: RecipeService 레시피 삭제 및 예외발생 테스트코드 작성

---
+++ src/test/java/project/bobzip/recipe/service/RecipeServiceDeleteTest.java
@@ -0,0 +1,71 @@
+package project.bobzip.recipe.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.bobzip.member.entity.Member;
+import project.bobzip.member.repository.MemberRepository;
+import project.bobzip.recipe.entity.Recipe;
+import project.bobzip.recipe.exception.UnauthorizedAccessException;
+import project.bobzip.recipe.repository.RecipeRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+public class RecipeServiceDeleteTest {
+
+    @Autowired
+    RecipeService recipeService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    Member writer;
+    Member noAuthorizer;
+
+    @BeforeEach
+    void init() {
+        writer = new Member("test", "123", "testMember");
+        noAuthorizer = new Member("new", "233", "newbie");
+        memberRepository.save(writer);
+        memberRepository.save(noAuthorizer);
+    }
+
+    @Test
+    void deleteRecipeTest() {
+        // given
+        Recipe testRecipe = createRecipeWithWriter("김치찌개", "맛있는 김치찌개를 끓여봅시다!", writer);
+        recipeRepository.save(testRecipe);
+
+        // when
+        recipeService.deleteRecipe(testRecipe.getId(), writer);
+
+        // then
+        Recipe findRecipe = recipeService.findRecipe(testRecipe.getId());
+        assertThat(findRecipe).isNull();
+    }
+
+    @Test
+    void noAuthorizerDeleteRecipeTest() {
+        // given
+        Recipe testRecipe = createRecipeWithWriter("김치찌개", "맛있는 김치찌개를 끓여봅시다!", writer);
+
+        // then
+        assertThatThrownBy(()->recipeService.deleteRecipe(testRecipe.getId(), noAuthorizer))
+                .isInstanceOf(UnauthorizedAccessException.class);
+    }
+
+
+    private Recipe createRecipeWithWriter(String title, String instruction, Member authorizer) {
+        return Recipe.builder()
+                .title(title)
+                .instruction(instruction)
+                .member(authorizer)
+                .build();
+    }
+}
