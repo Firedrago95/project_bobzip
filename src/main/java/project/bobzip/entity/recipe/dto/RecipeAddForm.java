81561ab77223ec5a9b556d8a0df40bbd534e022e
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-22 14:49:35 +0900

    Fix : DTO List 미리 초기화

--- src/main/java/project/bobzip/entity/recipe/dto/RecipeAddForm.java
+++ src/main/java/project/bobzip/entity/recipe/dto/RecipeAddForm.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,14 +22,14 @@ public class RecipeAddForm {
     @NotNull
     private MultipartFile thumbnail;
 
-    private List<@NotEmpty(message = "재료명을 입력해주세요")String> ingredientNames;
+    private List<@NotEmpty(message = "재료명을 입력해주세요")String> ingredientNames = new ArrayList<>();
 
-    private List<@NotNull(message = "재료수량을 입력해주세요")Integer> quantities;
+    private List<@NotNull(message = "재료수량을 입력해주세요")Integer> quantities = new ArrayList<>();
 
-    private List<@NotNull(message = "단위를 입력해주세요")String> units;
+    private List<@NotNull(message = "단위를 입력해주세요")String> units = new ArrayList<>();
 
-    private List<@NotEmpty(message = "조리법을 입력해주세요")String> stepInstructions;
+    private List<@NotEmpty(message = "조리법을 입력해주세요")String> stepInstructions = new ArrayList<>();
 
-    private List<@NotNull(message = "조리법 이미지를 등록해주세요")MultipartFile> stepThumbnails;
+    private List<@NotNull(message = "조리법 이미지를 등록해주세요")MultipartFile> stepThumbnails = new ArrayList<>();
 
 }
