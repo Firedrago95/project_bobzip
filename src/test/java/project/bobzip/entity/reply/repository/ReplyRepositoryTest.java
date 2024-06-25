6d8668809e028cb3a68d26ce1042667b366e7c31
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-25 12:02:44 +0900

    Test: 댓글 조회 테스트 변경

--- src/test/java/project/bobzip/entity/reply/repository/ReplyRepositoryTest.java
+++ src/test/java/project/bobzip/entity/reply/repository/ReplyRepositoryTest.java
@@ -6,7 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
+import project.bobzip.entity.member.entity.Member;
 import project.bobzip.entity.member.repository.MemberRepository;
+import project.bobzip.entity.recipe.entity.Recipe;
+import project.bobzip.entity.recipe.repository.RecipeRepository;
 import project.bobzip.entity.reply.entity.Reply;
 
 import java.util.Arrays;
@@ -22,27 +25,54 @@ class ReplyRepositoryTest {
     private ReplyRepository replyRepository;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private RecipeRepository recipeRepository;
 
     @Test
-    void findPagingReplyTest() {
+    void findByRecipeIdTest() {
         // given
-        List<Reply> testReplies = Arrays.asList(
-                Reply.builder()
-                .comment("정말 맛있는 요리입니다.")
-                .build(),
-                Reply.builder()
-                        .comment("조리법 대로 요리하니 간이 좀 맞지 않네요...")
-                        .build()
-                );
-        replyRepository.saveAll(testReplies);
+        Recipe recipe = createTestRecipe();
+        Member member = createTestMember();
+        createTestReplies(recipe, member);
         PageRequest pageable = PageRequest.of(0, 1);
 
         // when
-        Page<Reply> pagingReply = replyRepository.findAll(pageable);
+        Page<Reply> pagingReply = replyRepository.findByRecipeId(recipe.getId(), pageable);
 
         // then
         List<Reply> content = pagingReply.getContent();
         assertThat(pagingReply.getTotalPages()).isEqualTo(2);
         assertThat(content.size()).isEqualTo(1);
+    }
+
+    private void createTestReplies(Recipe recipe, Member member) {
+        List<Reply> testReplies = Arrays.asList(
+                Reply.builder()
+                        .recipe(recipe)
+                        .member(member)
+                        .comment("정말 맛있는 요리입니다.")
+                        .build(),
+                Reply.builder()
+                        .recipe(recipe)
+                        .member(member)
+                        .comment("조리법 대로 요리하니 간이 좀 맞지 않네요...")
+                        .build()
+        );
+        replyRepository.saveAll(testReplies);
+    }
+
+    private Member createTestMember() {
+        Member member = new Member("test", "123", "test");
+        memberRepository.save(member);
+        return member;
+    }
+
+    private Recipe createTestRecipe() {
+        Recipe recipe = Recipe.builder()
+                .title("된장찌개")
+                .instruction("맛있는 된장찌개")
+                .build();
+        recipeRepository.save(recipe);
+        return recipe;
     }
 }
