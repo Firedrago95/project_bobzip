44567b97fbbbedeeb03fe442507c328efc920132
YONGHWA\ekore <yonghwa95@naver.com>
2024-06-26 19:35:32 +0900

    Fix : 레시피 입력폼 수정, 동적 폼 생성 적용

--- src/main/resources/static/js/addForm.js
+++ src/main/resources/static/js/addForm.js
@@ -1,54 +1,70 @@
-var stepIndex = 1; // 과정 인덱스를 관리하기 위한 변수
-
-function readURL(input, id) {
+function readURL(input, targetId) {
     if (input.files && input.files[0]) {
         const reader = new FileReader();
         reader.onload = function(e) {
-            document.getElementById(id).src = e.target.result;
-        };
+            document.getElementById(targetId).src = e.target.result;
+        }
         reader.readAsDataURL(input.files[0]);
-    } else {
-        document.getElementById(id).src = "";
     }
 }
 
-function clearInputs(element) {
-    element.querySelectorAll("input, select").forEach(function(input) {
+function addIngredient(containerId) {
+    const container = document.getElementById(containerId);
+    const newForm = container.querySelector(".ingredientForm").cloneNode(true);
+    newForm.querySelectorAll("input").forEach(input => input.value = "");
+    container.appendChild(newForm);
+}
+
+function deleteIngredient(button, containerId) {
+    const container = document.getElementById(containerId);
+    if (container.querySelectorAll(".ingredientForm").length > 1) {
+        button.parentElement.parentElement.remove();
+    }
+}
+
+function addRecipeStep(containerId) {
+    const container = document.getElementById(containerId);
+    const newForm = container.querySelector(".cookingStepForm").cloneNode(true);
+
+    newForm.querySelectorAll("input, textarea").forEach(input => {
         input.value = "";
+        input.name = "";
     });
-    element.querySelectorAll("img").forEach(function(img) {
-        img.src = "";
+    newForm.querySelectorAll("img").forEach(input => {
+            input.src = "/image/noImage.png";
     });
+
+    container.appendChild(newForm);
+
+    updateStepNumbers(containerId);
 }
 
-function addIngredient(id) {
-    const ingredientList = document.getElementById(id);
-    const newIngredient = document.querySelector("#" + id + " li").cloneNode(true);
-    clearInputs(newIngredient);
-    ingredientList.appendChild(newIngredient);
-}
-
-function addRecipeStep(id) {
-    const recipeStepList = document.getElementById(id);
-    const newRecipeStep = document.querySelector("#" + id + " li").cloneNode(true);
-    clearInputs(newRecipeStep);
-
-    // 이미지 태그에 id 설정
-    const stepImageId = "step" + stepIndex;
-    newRecipeStep.querySelector("img").id = stepImageId;
-
-    // 이미지 입력 태그의 onchange 이벤트에 새로운 readURL 함수 적용
-    newRecipeStep.querySelector("input[type=file]").setAttribute("onchange", "readURL(this, '" + stepImageId + "')");
-
-    stepIndex++;
-    recipeStepList.appendChild(newRecipeStep);
-}
-
-function deleteForm(obj, id) {
-    const list = document.getElementById(id);
-    const childCount = list.querySelectorAll("li").length;
-
-    if (childCount > 1) {
-        list.removeChild(obj.parentNode);
+function deleteRecipeStep(button, containerId) {
+    const container = document.getElementById(containerId);
+    if (container.querySelectorAll(".cookingStepForm").length > 1) {
+        button.parentElement.parentElement.remove();
+        updateStepNumbers(containerId);
     }
 }
+
+function updateStepNumbers(containerId) {
+    const container = document.getElementById(containerId);
+    const steps = container.querySelectorAll(".cookingStepForm");
+
+    steps.forEach((step, index) => {
+        step.querySelector(".step-number").value = index + 1;
+        step.querySelector(".step-number").name = `steps[${index}].stepNumber`;
+
+        step.querySelector("textarea").name = `steps[${index}].instruction`;
+
+        const fileInput = step.querySelector("input[type=file]");
+        fileInput.name = `steps[${index}].thumbnail`;
+        fileInput.setAttribute("onchange", `readURL(this, 'step${index}')`);
+
+        step.querySelector(".img-thumbnail").id = `step${index}`;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateStepNumbers('cooking_step');
+});
