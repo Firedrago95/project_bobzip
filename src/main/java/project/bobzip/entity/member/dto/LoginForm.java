aabf06038dbdabd541e959dea122208e1a677f55
YONGHWA\ekore <yonghwa95@naver.com>
2024-07-20 12:20:01 +0900

    Feat : 회원 관련 dto에 @NotEmpty message 추가

--- src/main/java/project/bobzip/entity/member/dto/LoginForm.java
+++ src/main/java/project/bobzip/entity/member/dto/LoginForm.java
@@ -6,8 +6,8 @@ import lombok.Data;
 @Data
 public class LoginForm {
 
-    @NotEmpty
+    @NotEmpty(message = "아디디를 입력해주세요")
     private String userId;
-    @NotEmpty
+    @NotEmpty(message = "비밀번호를 입력해주세요")
     private String password;
 }
--- src/main/java/project/bobzip/entity/member/dto/SignForm.java
+++ src/main/java/project/bobzip/entity/member/dto/SignForm.java
@@ -1,15 +1,15 @@
 package project.bobzip.entity.member.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class SignForm {
 
-    @NotNull
+    @NotEmpty(message = "아이디를 입력해주세요")
     private String userId;
-    @NotNull
+    @NotEmpty(message = "비밀번호를 입력해주세요")
     private String password;
-    @NotNull
+    @NotEmpty(message = "닉네임을 입력해주세요")
     private String username;
 }
